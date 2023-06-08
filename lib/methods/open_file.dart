99befa6d4491a79f968b47f426f7d59bdd523058
Army NonCom Tools <armynoncomtools@gmail.com>
2023-06-08 19:56:50 +0900

    replace open file with open file plus

--- lib/methods/open_file.dart
+++ lib/methods/open_file.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:open_file/open_file.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:open_file_plus/open_file_plus.dart';
 
 void openFile(String filePath) async {
-  if (await Permission.manageExternalStorage.request().isGranted) {
-    final result = await OpenFile.open(filePath);
-    debugPrint('type: ${result.type}, message: ${result.message}');
-  }
+  final result = await OpenFile.open(filePath);
+  debugPrint('type: ${result.type}, message: ${result.message}');
 }
